a8c05632dcb9ed8d05e97bcd956474105f42f8f3
MaazinMunawar <78363373+MaazinMunawar@users.noreply.github.com>
2025-08-14 11:31:10 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! MY NAME IS MAAZIN"
+    return "Hello, World! AJKE 5TAR DIKE CSE471 PROJECT SHOWCASING ASE 9C-16T te"
 
 if __name__ == "__main__":
     app.run()
